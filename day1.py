113af2116c85adc9f6e6069af328f2f36208747b
Eva <smijakova@turing-technology.com>
2020-12-02 19:23:33 +0100

    Comment day 1

--- day1.py
+++ day1.py
@@ -1,3 +1,10 @@
+"""
+Day 1 is simple: the task is to find two (three resp.) numbers in a list which sum adds up to 2020.
+I decided to use this day to practice list comprehension for a bit.
+The solution could be slightly improved by filtering the empty lists from the output product.
+"""
+
+
 def part1():
     file = open("input1.txt")
     entries = [int(line) for line in file]
